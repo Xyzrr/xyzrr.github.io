1161853764efa69490aaf3a5b0fbd54724b3affc
John Qian <johnqian@xyzrr.attlocal.net>
2019-08-22 00:18:22 -0700

    render empty env and table

--- src/pages/QLearningPage.jsx
+++ src/pages/QLearningPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import * as colors from "../colors";
+import useWindowSize from "../util/useWindowSize";
 
 const Button = styled.button`
   padding: 8px;
@@ -27,16 +28,54 @@ const Page = styled.div`
 
 function QLearningPage() {
   const canvasRef = React.useRef(null);
+  const size = useWindowSize();
+
+  const [qTable, setQTable] = React.useState([
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
+  ]);
+  const [state, setState] = React.useState(0);
+
+  const drawEnvironment = ctx => {
+    ctx.strokeStyle = "cyan";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    for (let i = 0; i < 5; i++) {
+      ctx.lineTo(size.width / 2 - 200 + 100 * i - 25, 400);
+      ctx.moveTo(size.width / 2 - 200 + 100 * i + 25, 400);
+      ctx.arc(size.width / 2 - 200 + 100 * i, 400, 25, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+    // ctx.fill();
+  };
+
+  const drawQTable = ctx => {
+    ctx.lineWidth = 1;
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "cyan";
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 2; j++) {
+        ctx.beginPath();
+        ctx.rect(size.width / 2 - 100 + 100 * j, 50 + 50 * i, 100, 50);
+        ctx.stroke();
+        ctx.fillText(qTable[i][j], size.width / 2 - 90 + 100 * j, 86 + 50 * i);
+      }
+    }
+  };
 
   React.useEffect(() => {
     const canvas = canvasRef.current;
-    const canvasWidth = window.innerWidth;
-    const canvasHeight = window.innerHeight;
-    canvas.width = canvasWidth * window.devicePixelRatio;
-    canvas.height = canvasHeight * window.devicePixelRatio;
+    canvas.width = size.width * window.devicePixelRatio;
+    canvas.height = size.height * window.devicePixelRatio;
     const ctx = canvas.getContext("2d");
     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0, 0, size.width, size.height);
+
+    drawEnvironment(ctx);
+    drawQTable(ctx);
   });
 
   return (
