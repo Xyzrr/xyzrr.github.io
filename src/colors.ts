fe430a9c175620cb636a83c5366a18037bd1e87d
John Qian <johnqian@xyzrr.lan>
2019-09-15 15:55:09 -0700

    darken again

--- src/colors.ts
+++ src/colors.ts
@@ -1,8 +1,8 @@
 import Color from 'color';
 
-export const darkGray = Color.rgb(30, 30, 30);
+export const darkGray = Color.rgb(10, 10, 10);
 export const gray = Color.rgb(110, 110, 110);
-export const lightGray = Color.rgb(212, 212, 212);
+export const lightGray = Color.rgb(200, 200, 200);
 export const veryLightGray = Color.rgb(224, 224, 224);
 export const green = Color.rgb(186, 200, 146);
 export const darkGreen = Color.rgb(78, 201, 176);
@@ -13,8 +13,8 @@ export const purple = Color.rgb(197, 134, 192);
 export const yellow = Color.rgb(220, 220, 170);
 export const brown = Color.rgb(206, 145, 120);
 
-export const bgBrown = Color.rgb(69, 55, 21);
-export const bgGreen = Color.rgb(21, 69, 55);
+export const bgBrown = Color.rgb(54, 36, 17);
+export const bgGreen = Color.rgb(17, 54, 31);
 
 export const QLearningColors = {
   state: purple,
