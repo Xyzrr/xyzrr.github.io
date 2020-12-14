cc6abc6f261b240feb04e1cef579692f89ca8c35
John Qian <johnlongqian@gmail.com>
2020-12-14 14:08:09 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9ed1574af025d803205cf23bd3b0b9a.js"
+  "/precache-manifest.5d80daed1245de622017f5a765305fc6.js"
 );
 
 self.addEventListener('message', (event) => {
