d39234d037ecf7577f2d2c0ea2ef2cb4ae8fd60d
John Qian <johnqian@Johns-MacBook-Pro-2.local>
2025-04-02 14:43:31 -0700

    x profile, no gains

--- client/src/portfolio/pages/HomePage.tsx
+++ client/src/portfolio/pages/HomePage.tsx
@@ -126,9 +126,23 @@ const HomePage: React.FC = (props) => {
           <span>∙</span>
           <a href="https://blog.johnqian.com">Blog</a>
           <span>∙</span>
-          <a href="/gains">Gains</a>
-          <span>∙</span>
           <a href="mailto:johnlongqian+site@gmail.com">Contact</a>
+          <span>∙</span>
+          <a
+            href="https://x.com/johnlqian"
+            target="_blank"
+            aria-label="X Profile"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+            </svg>
+          </a>
         </Links>
       </div>
       <div className="footer">
